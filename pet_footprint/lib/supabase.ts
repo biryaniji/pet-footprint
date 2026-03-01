348bf67670d63e446013240bd534ea89f4341408
biryaniji <kabiranand2901@gmail.com>
2026-03-02 01:09:15 +0530

    hardcode supabase credentials

--- pet_footprint/lib/supabase.ts
+++ pet_footprint/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://oudmrgwwollcxxxfrpik.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91ZG1yZ3d3b2xsY3h4eGZycGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTIxMzcsImV4cCI6MjA4Nzc4ODEzN30.f4k2BofMJkk3Mz1zkTWX8qmzbhN3sGUGOnLPk2CdxAo"
 )
