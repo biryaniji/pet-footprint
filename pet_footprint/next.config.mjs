6558508c0f40056b560a20167bec012893b13bf2
biryaniji <kabiranand2901@gmail.com>
2026-03-03 14:08:53 +0530

    fix cors headers

--- pet_footprint/next.config.mjs
+++ pet_footprint/next.config.mjs
@@ -4,7 +4,33 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oudmrgwwollcxxxfrpik.supabase.co",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ]
   },
 }
 
